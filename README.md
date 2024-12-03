2036a4c5d08386b428c336706b7df987f869ddb6
Randall Hand <randall.hand@gmail.com>
2024-12-03 13:44:17 -0500

    Initial checkins

---
+++ README.md
@@ -0,0 +1,17 @@
+# Requirements
+pip install deluge-client
+
+
+# To run the script
+
+First edit the config-sample.ini file and save it as config.ini
+Then just run block.ini
+
+
+# What it does
+
+Torrents that contain files with extensions in the "Forbidden" list will automatically be _removed_ from deluge.  This is to stop viruses or other unwanted content.
+
+Torrents that contain files in the "unwanted" list will have those specific files flagged to Skip, but the torrent will be left alone.  This is to disable unwanted artwork, text files, etc that may come along with a torrent.
+
+The script will check deluge every 60s to apply changes.
---
+++ block.py
@@ -0,0 +1,92 @@
+from deluge_client import DelugeRPCClient
+import time
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# Connect to Deluge
+client = DelugeRPCClient(
+    config["login"]["host"],
+    int(config["login"]["port"]),
+    config["login"]["user"],
+    config["login"]["password"],
+)
+
+
+# Specify the forbidden file extensions
+forbidden_extensions = config["lists"]["forbidden"].split(",")
+unwanted_extensions = config["lists"]["unwanted"].split(",")
+
+print("Scanning..")
+print(" -> %i forbidden extensions" % len(forbidden_extensions))
+print(" -> %i unwanted extensions" % len(unwanted_extensions))
+
+
+# Function to check for forbidden files and remove the torrent
+def check_and_remove_torrents():
+    torrents = client.call(
+        "core.get_torrents_status", {}, [b"name", b"files", b"file_priorities"]
+    )
+
+    for torrent_id, torrent_data in torrents.items():
+        # Decode byte keys
+        torrent_name = torrent_data.get(b"name", b"Unknown Name").decode(
+            "utf-8", errors="ignore"
+        )
+        files = torrent_data.get(b"files", [])
+        file_priorities = torrent_data.get(b"file_priorities", [])
+        print(f"Validating {torrent_name}")
+
+        # If no files are listed, skip this torrent
+        if not files:
+            print(
+                f"No files found for torrent '{torrent_name}' (ID: {torrent_id}), skipping."
+            )
+            continue
+
+        # Check each file in the torrent for unwanted extensions
+        priorities = []
+        forbidden = False
+        forbidden_filename = ""
+        for file_info, file_priority in zip(files, file_priorities):
+            file_name = file_info.get(b"path", b"").decode("utf-8", errors="ignore")
+            if file_priority > 0:
+                if any(file_name.endswith(ext) for ext in unwanted_extensions):
+                    print(f"-> Skipping {file_name}")
+                    priorities.append(0)  # skip
+                else:
+                    priorities.append(file_priority)
+            else:
+                priorities.append(file_priority)
+                # Remove the torrent
+            if any(file_name.endswith(ext) for ext in forbidden_extensions):
+                # mark this, and abort further checks
+                forbidden_filename = file_name
+                forbidden = True
+                break
+
+        if forbidden:
+            print(
+                f"Removing torrent '{torrent_name}' due to forbidden file '{forbidden_filename}'"
+            )
+            # Remove the torrent
+            client.call(
+                "core.remove_torrent",
+                torrent_id.decode("utf-8", errors="ignore"),
+                True,
+            )
+        else:
+            client.call(
+                "core.set_torrent_options",
+                torrent_id.decode("utf-8", errors="ignore"),
+                {"file_priorities": priorities},
+            )
+
+
+# Run the function
+
+while 1:
+    check_and_remove_torrents()
+    time.sleep(60)
